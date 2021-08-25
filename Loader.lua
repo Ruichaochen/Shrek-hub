c64194fe6eb9be46b4a384413226f522210979a4
Ruichaochen <65166963+Ruichaochen@users.noreply.github.com>
2021-08-25 17:01:27 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,4 @@
 if game.PlaceId == 6706152946 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruichaochen/Shrek-hub/main/CrusadersHaven.lua',true))()
+  else game.Players.LocalPlayer:Kick("Wrong game")
   end
